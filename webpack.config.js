b412809904d7b320e5db011678d93e8b7c12144f
justusromijn <justus.romijn@gmail.com>
2017-07-18 22:12:57 +0200

    WIP setup fonts inclusion configuration

--- webpack.config.js
+++ webpack.config.js
@@ -52,6 +52,10 @@ module.exports = {
                     //use style-loader in development
                     fallback: "style-loader"
                 })
+            },
+            {
+                test: /\.(eot|svg|ttf|woff(2)?)(\?v=\d+\.\d+\.\d+)?/,
+                loader: 'url-loader'
             }
         ]
     }
